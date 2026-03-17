b988a9e06c950320ce68f1acde50c6edd219eb93
maxime99996 <maxogalo15@gmail.com>
2026-03-17 11:36:06 +0100

    savon-APP

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { Footer } from './components/footer/footer';
+import { Header } from './components/header/header';
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+selector: 'app-root',
+imports: [RouterOutlet, Header, Footer], // Remplacement de Navbar par Header
+templateUrl: './app.html',
+styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('sav-app');
+protected readonly title = signal('sav-app');
 }
